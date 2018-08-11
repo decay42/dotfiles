7431d33736e1fe81a5b969c23a3465a8f5ef4ff1
Jan Pfeiffer <liberry@ymail.com>
2018-08-11 23:18:42 +0200

    Update

--- setup.sh
+++ setup.sh
@@ -9,28 +9,27 @@ else
   exit
 fi
 
-ln -s ./dircolors ~/.dircolors
-ln -s ./gitconfig ~/.gitconfig
-ln -s ./tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles2/dircolors ~/.dircolors
+ln -s ~/dotfiles2/gitconfig ~/.gitconfig
+ln -s ~/dotfiles2/tmux.conf ~/.tmux.conf
 
 if [ -f ~/.zshrc ]; then
   mv ~/.zshrc ~/.zshrc.old
 fi
-ln -s ./zshrc ~/.zshrc
+ln -s ~/dotfiles2/zshrc ~/.zshrc
 
 if [ -f ~/.vimrc ]; then
   mv ~/.vimrc ~/.vimrc.old
 fi
-ln -s ./vimrc ~/.vimrc
+ln -s ~/dotfiles2/vimrc ~/.vimrc
 
 mkdir -p ~/local/share/nvim/plugged
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s ./init.vim ~/.config/nvim/init.vim
+ln -s ~/dotfiles2/init.vim ~/.config/nvim/init.vim
 
 mkdir -p ~/.config/terminator
-ln -s ./terminator.config ~/.config/terminator/config
+ln -s ~/dotfiles2/terminator.config ~/.config/terminator/config
 
 mkdir -p ~/.fonts
-ln -s ./Hack-Regular.ttf ~/.fonts/Hack-Regular.ttf
-
+ln -s ~/dotfiles2/Hack-Regular.ttf ~/.fonts/Hack-Regular.ttf
